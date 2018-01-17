47a1fb24ad2215354fa8c6db547f1683c0252975
SarahSchaab <sarahschaab@me.com>
2018-01-17 11:15:41 -0500

    add user auth, and registration error handling

--- app.js
+++ app.js
@@ -1,6 +1,23 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const session = require('express-session');
 const app = express();
+
+// use sessions for tracking logins
+app.use(session({
+  secret: "this is a secret",
+  resave: true,
+  saveUninitialized: false
+}));
+
+
+// mongodb connection
+mongoose.connect("mongodb://localhost:27017/skatespot");
+var db = mongoose.connection;
+
+// mongodb error handling
+db.on('error', console.error.bind(console, 'connection error:'));
 
 // parse incoming requests
 app.use(bodyParser.json());
